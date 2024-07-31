7419a505862b2ab1b8d08dcf50dc3a9bb84714c3
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-07-31 00:03:32 +0000

    initial commit

---
+++ app/Template/Footer.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Footer Class.
+ *
+ * A simple class for outputting the appropriate footer credit text.
+ *
+ * @package   Amicable
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/amicable
+ */
+
+namespace Amicable\Template;
+
+use Amicable\Tools\Mod;
+use Amicable\Tools\PoweredBy;
+
+/**
+ * Powered by class.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class Footer {
+
+	/**
+	 * Displays a random powered by quote.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public static function displayCredit() {
+
+		echo static::renderCredit();
+	}
+
+	/**
+	 * Returns a random powered by quote.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return string
+	 */
+	public static function renderCredit( array $args = [] ) {
+
+		$args = wp_parse_args( $args, [
+			'before' => '<p class="site-footer-credit">',
+			'after'  => '</p>'
+		] );
+
+		$text = Mod::get( 'powered_by' )
+		        ? PoweredBy::render()
+			: Mod::get( 'footer_credit' );
+
+		return sprintf(
+			'%s%s%s',
+			$args['before'],
+			wp_kses( $text, static::allowedTags() ),
+			$args['after']
+		);
+	}
+
+	/**
+	 * Returns an array of allowed tags in footer text.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return array
+	 */
+	public static function allowedTags() {
+
+		return [
+			'a'       => [ 'href' => true, 'title' => true, 'class' => true ],
+			'abbr'    => [ 'title' => true ],
+			'acronym' => [ 'title' => true ],
+			'bold'    => [ 'class' => true ],
+			'code'    => [ 'class' => true ],
+			'em'      => [ 'class' => true ],
+			'i'       => [ 'class' => true ],
+			'span'    => [ 'class' => true ],
+			'strong'  => [ 'class' => true ]
+		];
+	}
+}
